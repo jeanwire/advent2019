ff56919e49bfcbd7ea67dc4719f29c59cf7ab685
Kathleen McGuire <kathleen.macg@gmail.com>
2019-12-01 11:35:24 -0500

    second puzzle

--- day1_p1.py
+++ day1_p1.py
@@ -1,11 +1,14 @@
-
-
 def main():
     fuel_total = 0
     with open('input_day_1.txt') as f:
         modules = f.readlines()
         for i in modules:
-            fuel_total += int(i) // 3 - 2
+            module_fuel = int(i) // 3 - 2
+            fuel_total += module_fuel
+            while module_fuel > 0:
+                module_fuel = module_fuel // 3 - 2
+                if module_fuel > 0:
+                    fuel_total += module_fuel
 
     print(fuel_total)
 
