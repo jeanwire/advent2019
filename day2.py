9ba393862752c3a0076e68dd7b229e6e3d9f5ec1
Kathleen McGuire <kathleen.macg@gmail.com>
2019-12-02 22:38:45 -0500

    second puzzle, day 2. Just brute forced it

--- day2.py
+++ day2.py
@@ -1,26 +1,39 @@
 
 def main():
-    i = 0
     with open('input_day_2.txt') as f:
-        input = f.readline().strip().split(',')
-        input = [int(i) for i in input]
+        program = f.readline().strip().split(',')
+        program = [int(i) for i in program]
 
-        while i < len(input) - 3:
-            if input[i] == 99:
+        solved = False
+
+        for j in range(0,100):
+            for k in range(0,100):
+                input = program.copy()
+                i = 0
+                input[1] = j
+                input[2] = k
+
+                while i < len(input) - 3:
+                    if input[i] == 99:
+                        break
+
+                    pos1 = input[i + 1]
+                    pos2 = input[i + 2]
+                    pos3 = input[i + 3]
+
+                    if input[i] == 1:
+                        input[pos3] = input[pos1] + input[pos2]
+                    elif input[i] == 2:
+                        input[pos3] = input[pos1] * input[pos2]
+
+                    i += 4
+
+                if input[0] == 19690720:
+                    solved = True
+                    print('SOLVED: ' , j, ' ', k)
+
+            if solved:
                 break
-
-            pos1 = input[i + 1]
-            pos2 = input[i + 2]
-            pos3 = input[i + 3]
-
-            if input[i] == 1:
-                input[pos3] = input[pos1] + input[pos2]
-            elif input[i] == 2:
-                input[pos3] = input[pos1] * input[pos2]
-
-            i += 4
-
-    print(input)
 
 
 if __name__ == '__main__':
